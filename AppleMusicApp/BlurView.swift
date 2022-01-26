dad9545c62aaaf27ea0905e1a66d4cc841e7354c
GimmyFox <mguzeev99@gmail.com>
2022-01-27 01:38:41 +0300

    add miniplayer

---
+++ AppleMusicApp/BlurView.swift
@@ -0,0 +1,26 @@
+//
+//  BlurView.swift
+//  AppleMusicApp
+//
+//  Created by Maksim Guzeev on 27.01.2022.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+    
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
