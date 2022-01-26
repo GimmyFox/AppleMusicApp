1a479f89466ef53dfc6f7469019172d7eec378fc
GimmyFox <mguzeev99@gmail.com>
2022-01-27 01:19:37 +0300

    add tabBar / libraryView

---
+++ LibraryView.swift
@@ -0,0 +1,25 @@
+//
+//  LibraryView.swift
+//  AppleMusicApp
+//
+//  Created by Maksim Guzeev on 27.01.2022.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                
+            }
+            .navigationTitle("Медиатека")
+        }
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
---
+++ TabBar.swift
@@ -0,0 +1,43 @@
+//
+//  TabBar.swift
+//  AppleMusicApp
+//
+//  Created by Maksim Guzeev on 27.01.2022.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
+            
+            TabView {
+                LibraryView()
+                    .tabItem {
+                        Text("Медиатека")
+                        Image(systemName: "rectangle.stack.fill")
+                    }
+                Text("Radio")
+                    .tabItem {
+                        Text("Радио")
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                    }
+                Text("Search")
+                    .tabItem {
+                        Text("Поиск")
+                        Image(systemName: "magnifyingglass")
+                    }
+            }
+            
+
+
+        })
+    }
+}
+
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
