9bb1ec40c55aecd14147b27f01b523ae661a5292
GimmyFox <mguzeev99@gmail.com>
2022-01-27 01:02:20 +0300

    Initial Commit

---
+++ AppleMusicApp/AppleMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicAppApp.swift
+//  AppleMusicApp
+//
+//  Created by Maksim Guzeev on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusicApp
+//
+//  Created by Maksim Guzeev on 27.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
