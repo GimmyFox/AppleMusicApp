707ed155fedaf9be0dbc27b813b9290fe05fbcec
GimmyFox <mguzeev99@gmail.com>
2022-01-27 01:50:35 +0300

    completing code

--- AppleMusicApp/LibraryView.swift
+++ AppleMusicApp/LibraryView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct LibraryView: View {
+    
+    @State private var isEmptyLibrary = true
+    
     var body: some View {
         NavigationView {
             ZStack {
-                
+                if isEmptyLibrary {
+                    EmptyView()
+                } else {
+                    FilterView()
+                }
             }
             .navigationTitle("Медиатека")
+            .environment(\.editMode, .constant(
+                isEmptyLibrary ? EditMode.inactive : EditMode.active))
+            .accentColor(.red)
+        
+            .navigationBarItems(trailing: Button(action: {
+                isEmptyLibrary.toggle()
+            }, label: {
+                if isEmptyLibrary {
+                    Text("Править")
+                        .foregroundColor(.red)
+                } else {
+                    Text("Готово")
+                        .foregroundColor(.red)
+                }
+            }))
         }
     }
 }
